1b44bb8e27ab87c207d12c621ab3918b9660118c
BulletKin <100952-BulletKin@users.noreply.gitgud.io>
2026-01-29 18:09:43 -0500

    Added GuildMember, and Primary Guild structs. Added more fields in the User struct.

--- types.go
+++ types.go
@@ -32,11 +32,33 @@ type Message struct {
 }
 
 type User struct {
-	ID            string `json:"id"`
-	Username      string `json:"username"`
-	Discriminator string `json:"discriminator"`
-	Avatar        string `json:"avatar"`
-	Bot           bool   `json:"bot,omitempty"`
+	ID            string       `json:"id"`
+	Username      string       `json:"username"`
+	Discriminator string       `json:"discriminator"`
+	Avatar        string       `json:"avatar"`
+	Bot           bool         `json:"bot,omitempty"`
+	Email         string       `json:"email,omitempty"` // I am guessing this can only be used by Discord employees.
+	Flags         int          `json:"flags,omitempty"`
+	PremiumTypes  int          `json:"premium_type,omitempty"`
+	PrimaryGuild  PrimaryGuild `json:"primary_guild,omitempty"`
+}
+
+type GuildMember struct { // Data that is tied to a user within a guild.
+	User              User   `json:"user,omitempty"`
+	Nickname          string `json:"nick,omitempty"`
+	Avatar            string `json:"avatar,omitempty"`        // Hash
+	Banner            string `json:"banner,omitempty"`        // Hash
+	DateJoined        string `json:"joined_at"`               // ISO8601 timestamp
+	PremiumTime       string `json:"premium_since,omitempty"` // ISO8601 timestamp
+	Flags             int    `json:"flags"`
+	TimeoutExpiration string `json:"communication_disabled_until,omitempty"` // ISO8601 timestamp
+}
+
+type PrimaryGuild struct { // This could prove useful for traversing guilds
+	GuildId         string `json:"identity_guild_id"`
+	IdentityEnabled bool   `json:"identity_enabled"` // null if identity is clear, false if manually removed by guild
+	Tag             string `json:"tag"`              // 4 letter text.
+	Badge           string `json:"badge"`            // hash
 }
 
 type Attachment struct {
