b7312786276b7b1b13ce8a1150ca77115b6710e6
BulletKin <BulletKin2@proton.me>
2026-01-30 04:32:32 -0500

    got this one helper function instead.

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+func initDatabase() *Database {
+	db, err := NewDatabase(dbPath)
+	if err != nil {
+		fmt.Printf("❌ Error initializing database: %v\n", err)
+		os.Exit(1)
+	}
+	return db
+}
+
 func main() {
-	Execute()
+
+	// Execute()
 }
