381dabf3f4c3aee709d434c42885765f1d019a08
BulletKin <100952-BulletKin@users.noreply.gitgud.io>
2026-01-31 05:41:44 -0500

    some tests

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func initDatabase() *Database {
-	db, err := NewDatabase(dbPath)
+	db, err := NewDatabase("discord_archive.db")
 	if err != nil {
 		fmt.Printf("❌ Error initializing database: %v\n", err)
 		os.Exit(1)
@@ -15,6 +17,27 @@ func initDatabase() *Database {
 }
 
 func main() {
+	// set up env for testing
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Println("Just a quick test")
+
+	db := initDatabase()
 	// Execute()
+
+	fmt.Println("Testing.")
+	auth := os.Getenv("authorization")
+	fmt.Println(auth)
+	arc := NewArchiver(auth, db)
+
+	fmt.Println("Testing for channels")
+	chans, err := arc.GetChannel("1465598299074592906")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(chans)
 }
